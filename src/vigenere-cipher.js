db13ac11c5c6bab7a17b1b4ed593bd70a172a408
beaghmore <beaghmore.yandex.ru>
2022-10-16 17:05:23 -0400

    add: cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct=true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if ((arguments.length < 2) || (arguments[0] == undefined) || (arguments[0] == undefined)) throw "Incorrect arguments!";
+  
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    console.log(key);
+  
+    let result = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++){
+      if (this.isLetter(message[i])) {
+        let ordChar = ((message[i].charCodeAt(0) - "A".charCodeAt(0)) + (key[j].charCodeAt(0) - "A".charCodeAt(0))) % 26 + "A".charCodeAt(0);
+        let char = String.fromCharCode(ordChar);
+        result += char;
+        j += 1;
+        if (j === key.length) j = 0;
+      } else {
+        result += message[i];
+      }
+    }
+    console.log(result);
+
+    if (this.direct === false) result = this.reverseString(result);
+    return result;
+  }
+  
+  decrypt(message, key) {
+    if ((arguments.length < 2) || (arguments[0] == undefined) || (arguments[0] == undefined)) throw "Incorrect arguments!";
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    console.log(key);
+  
+    let result = "";
+    let j = 0;
+    for (let i = 0; i < message.length; i++){
+      if (this.isLetter(message[i])) {
+        let ordChar = ((message[i].charCodeAt(0) - "A".charCodeAt(0)) - (key[j].charCodeAt(0) - "A".charCodeAt(0)));
+        if (ordChar < 0) ordChar = 26 + ordChar;
+        console.log(message[i].charCodeAt(0) - "A".charCodeAt(0), (key[j].charCodeAt(0) - "A".charCodeAt(0)), ordChar);
+        let char = String.fromCharCode(ordChar + "A".charCodeAt(0));
+        result += char;
+        j += 1;
+        if (j === key.length) j = 0;
+      } else {
+        result += message[i];
+      }
+    }
+    
+    if (this.direct === false) result = this.reverseString(result);
+    console.log(result);
+    return result;
+  }
+  
+  isLetter(str) {
+    return str.length === 1 && str.match(/[A-Z]/i);
+  }
+
+  reverseString(str) {
+    return str.split("").reverse().join("");  
   }
 }
 
